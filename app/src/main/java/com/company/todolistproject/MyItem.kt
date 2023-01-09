ea640a10b27555b9beae6b2e46f0421052cc3b51
forceindia712 <albertbilskii@gmail.com>
2023-01-09 17:41:55 +0100

    Wykorzystanie standardu JSON - zadanie 8

--- app/src/main/java/com/company/todolistproject/MyItem.kt
+++ app/src/main/java/com/company/todolistproject/MyItem.kt
@@ -1,6 +1,8 @@
 package com.company.todolistproject
 
 data class MyItem (
-    val string: String?,
-    val link: Boolean
+    val id: Long = 0,
+    var data: String = "",
+    var text: String = "",
+    var isDeleted: Boolean = true,
 )
